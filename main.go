1a7c6780fbf266e59727ef4cd6e3ee7771189094
Fisherman August <fshrmn.august@gmail.com>
2025-08-11 20:04:04 +0300

    Gin later

--- main.go
+++ main.go
@@ -10,10 +10,31 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello from Gin Gonic Go!")
+		c.JSON(http.StatusOK, gin.H{
+			"Name": "Sergey",
+			"Age":  66,
+		})
 	})
-	r.GET("/user", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello from user!")
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello, %s!", name)
+	})
+
+	r.POST("/user", func(c *gin.Context) {
+		type User struct {
+			Name  string
+			Email string
+		}
+		var user User
+
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"Status": "Ok!",
+		})
+
 	})
 
 	r.Run(":8080")
