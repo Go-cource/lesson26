aa610fc02dc61956e97052745228c262383f52b0
August Fisherman <fshrmn.august@gmail.com>
2026-01-15 20:13:17 +0300

    Get Params

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,8 +11,16 @@ func indexHandler(c *gin.Context) {
 	c.String(http.StatusOK, "Hello, World!!")
 }
 
+func helloUser(c *gin.Context) {
+	c.String(http.StatusOK, fmt.Sprintf("Hello, %s", c.Param("name")))
+}
+
+
+
 func main() {
 	r := gin.Default()
 	r.GET("/", indexHandler)
+	r.GET("/user/:name", helloUser)
+	
 	r.Run(":8080")
 }
