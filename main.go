3a9b02c7eddba814fd5e02929152324e9b5ddd9b
August Fisherman <fshrmn.august@gmail.com>
2026-01-15 19:45:56 +0300

    hello gin)

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func indexHandler(c *gin.Context) {
+	c.String(http.StatusOK, "Hello, World!!")
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", indexHandler)
+	r.Run(":8080")
 }
