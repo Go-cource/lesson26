8fe17c37be6fb9718ec117f9d684d5d3b476cc23
August Fisherman <fshrmn.august@gmail.com>
2026-01-15 20:21:20 +0300

    POST json auth handler

--- main.go
+++ main.go
@@ -15,12 +15,32 @@ func helloUser(c *gin.Context) {
 	c.String(http.StatusOK, fmt.Sprintf("Hello, %s", c.Param("name")))
 }
 
+type Credo struct {
+	Email    string
+	Password string
+}
 
+func authHandler(c *gin.Context) {
+	var credo Credo
+	err := c.ShouldBindJSON(&credo)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"authError": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"access": "allowed",
+		"token":  "wdjnWubdnkaIIw123",
+	})
+
+}
 
 func main() {
 	r := gin.Default()
 	r.GET("/", indexHandler)
 	r.GET("/user/:name", helloUser)
-	
+	r.POST("/auth", authHandler)
+
 	r.Run(":8080")
 }
