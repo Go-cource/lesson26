23b4ad943e08d9691b93be9d2fdf5e738bdbbafb
August Fisherman <fshrmn.august@gmail.com>
2026-01-15 21:22:49 +0300

    godoc

--- main.go
+++ main.go
@@ -1,4 +1,9 @@
-package main
+// Это хорошее тестовое API для демонстрации работы Gin
+// # Обзор
+// Это занятие №26 на курсе ЦДО МГТУ им.Баумана. Мы учимся документировать проекты
+// Запуск go run main.go
+// Запуск клиента --------
+package ginModule
 
 import (
 	"fmt"
@@ -7,20 +12,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func indexHandler(c *gin.Context) {
+// indexHandler - это ручка для вывода "Hello, World!!"
+// Чтобы обратиться перейдите http://127.0.0.1:8080/
+func IndexHandler(c *gin.Context) {
 	c.String(http.StatusOK, "Hello, World!!")
 }
 
-func helloUser(c *gin.Context) {
+// helloUser - это ручка для вывода имени.
+// Обращаться: http://localhost:8080/user/:name, имя, тут будет выведено
+func HelloUser(c *gin.Context) {
 	c.String(http.StatusOK, fmt.Sprintf("Hello, %s", c.Param("name")))
 }
 
+// Credo - это структура для аутентификации.
+// Поля: Email (string), Password (string)
 type Credo struct {
 	Email    string
 	Password string
 }
 
-func authHandler(c *gin.Context) {
+// authHandler - это ручка, которая отвечает за аутентификацию пользователя по паролю.
+// Работает POST метод. Принимает структуру "Credo"
+// В случе неудачи: Status Bad Request.
+func AuthHandler(c *gin.Context) {
 	var credo Credo
 	err := c.ShouldBindJSON(&credo)
 	if err != nil {
@@ -38,9 +52,9 @@ func authHandler(c *gin.Context) {
 
 func main() {
 	r := gin.Default()
-	r.GET("/", indexHandler)
-	r.GET("/user/:name", helloUser)
-	r.POST("/auth", authHandler)
+	r.GET("/", IndexHandler)
+	r.GET("/user/:name", HelloUser)
+	r.POST("/auth", AuthHandler)
 
 	r.Run(":8080")
 }
